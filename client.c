439335cacb38f19f175b926ff17938d1bf003086
semidetached <semidetached@minime.org>
2012-12-06 19:22:06 -0500

    made client a cli tool for reading/writing/testing/killing

--- client.c
+++ client.c
@@ -2,7 +2,7 @@
 
 #include "client_api.h"
 
-int main(int argc, char **argv) {
+static void run_test(char *addr, char *port) {
     int i;
     char buf[8192];
 
@@ -13,14 +13,9 @@ int main(int argc, char **argv) {
     char *data;
     int fd;
 
-    if (argc < 3) {
-        printf("Usage: client <addr> <port>\n");
-        return -1;
-    }
-
-    fd = dht_connect(argv[1], argv[2]);
+    fd = dht_connect(addr, port);
     
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < 20; i++) {
         
         key_string[3]   = '0' + i;
         value_string[7] = '0' + i;
@@ -31,7 +26,76 @@ int main(int argc, char **argv) {
     key_string[3] = '0' + 5;
     dht_read(fd, key_string, buf);
 
-    printf("Got: %s\n", buf);
+    printf("Got: %s... ", buf);
+    if (0 == strcmp (buf, "abcdefg5"))
+        printf("OK\n");
+    else
+        printf("FAILED\n");
+
+    dht_kill(fd);
+    dht_disconnect(fd);
+
+}
+
+int main(int argc, char **argv) {
+    char buf[8192];
+    int fd;
+
+    if (argc < 2) {
+        printf("Usage: client <cmd> <args...>\n");
+        return -1;
+    }
+
+    switch (argv[1][0]) {
+        
+        case ('t'):
+            if (argc < 4) {
+                printf("Not enough arguments for the test\n");
+                return -1;
+            }
+
+            run_test(argv[2], argv[3]);
+            break;
+
+        case ('r'):
+            if (argc < 5) {
+                printf("Not enough arguments for a read\n");
+                return -1;
+            }
+            
+            fd = dht_connect(argv[2], argv[3]);
+            dht_read(fd, argv[4], buf);
+            printf("\nReceived: %s\n", buf);
+            dht_disconnect(fd);
+            break;
+
+        case ('w'):
+            if (argc < 6) {
+                printf("Not enough arguments for a write\n");
+                return -1;
+            }
+            
+            fd = dht_connect(argv[2], argv[3]);
+            dht_write(fd, argv[4], argv[5]);
+            dht_disconnect(fd);
+
+        case ('k'):
+            if (argc < 4) {
+                printf("Not enough arguments for kill\n");
+                return -1;
+            }
+
+            fd = dht_connect(argv[2], argv[3]);
+            dht_kill(fd);
+            printf("\nKilled DHT\n");
+            dht_disconnect(fd);
+
+            break;
+
+        default:
+            break;
+    }
 
     return 0;
 }
+
